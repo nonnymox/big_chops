181fb47646f9aac2a74f7ae3c9c8a03d1005f3a6
nonnymox <knonxho@gmail.com>
2025-02-16 02:33:15 -0800

    Addded a dummy template and created the superuser admin profile

--- big_chops/settings.py
+++ big_chops/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'big_chops.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
