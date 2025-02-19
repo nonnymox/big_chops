02b2fc326ab5998318c371276f327ee5f104dd02
nonnymox <knonxho@gmail.com>
2025-02-19 09:46:36 -0800

    Created the admmin models UserManager and User models on accounts app

---
+++ accounts/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+# Customizing the UserAdmin
+class CustomUserAdmin(UserAdmin):
+    list_display = ('first_name', 'last_name', 'username', 'email', 'role', 'is_admin', 'is_staff')
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    list_filter = ('role', 'is_admin', 'is_staff', 'is_active')
+    ordering = ('-date_joined',)
+    
+    fieldsets = (
+        ('Personal Information', {'fields': ('first_name', 'last_name', 'username', 'email', 'phone_number', 'role')}),
+        ('Permissions', {'fields': ('is_admin', 'is_staff', 'is_active', 'is_superuser')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('first_name', 'last_name', 'username', 'email', 'password1', 'password2', 'role', 'is_admin', 'is_staff', 'is_active'),
+        }),
+    )
+
+admin.site.register(User, CustomUserAdmin)
